78644c01ecf6b2000059b46d404e844caede90df
yannS2016 <mandza.y.s@gmail.com>
2017-09-07 16:21:37 +0200

    Updated SSC basement SNC source

--- databases/st.cmd
+++ databases/st.cmd
@@ -27,23 +27,29 @@ iocInit
 
 ## Start any sequence programs
 
-#seq ev1ClearanceSNC, "SYS=VCS"
-#seq ev2ClearanceSNC, "SYS=VCS"
-#seq ev3ClearanceSNC, "SYS=VCS"
-#seq bsvClearanceSNC, "SYS=VCS"
-#seq hliClearanceSNC, "SYS=VCS"
-#seq speClearanceSNC, "SYS=VCS"
-#seq vliClearanceSNC, "SYS=VCS"
-#seq a13aClearanceSNC, "SYS=VCS"
-#seq iblClearanceSNC, "SYS=VCS"
-#seq ssccabClearanceSNC, "SYS=VCS"
-#seq sscbasClearanceSNC, "SYS=VCS"
-#seq sscgfa1ClearanceSNC, "SYS=VCS"
-#seq sscgfa2ClearanceSNC, "SYS=VCS"
-#seq expClearanceSNC, "SYS=VCS"
 seq spc1gfClearanceSNC, "SYS=VCS"
 seq spc1basClearanceSNC, "SYS=VCS"
-#seq spc2basClearanceSNC, "SYS=VCS"
-#seq spc2gfClearanceSNC, "SYS=VCS"
-#seq trlClearanceSNC, "SYS=VCS"
-#seq trlClearanceSNC, "SYS=VCS"
+
+seq ssccabClearanceSNC, "SYS=VCS"
+seq sscbasClearanceSNC, "SYS=VCS"
+seq sscgfa1ClearanceSNC, "SYS=VCS"
+seq sscgfa2ClearanceSNC, "SYS=VCS"
+seq a13aClearanceSNC, "SYS=VCS"
+seq iblClearanceSNC, "SYS=VCS"
+
+seq expClearanceSNC, "SYS=VCS"
+seq ev1ClearanceSNC, "SYS=VCS"
+seq ev2ClearanceSNC, "SYS=VCS"
+seq ev3ClearanceSNC, "SYS=VCS"
+
+seq speClearanceSNC, "SYS=VCS"
+seq bsvClearanceSNC, "SYS=VCS"
+seq cnaClearanceSNC, "SYS=VCS"
+
+seq spc2basClearanceSNC, "SYS=VCS"
+seq spc2gfClearanceSNC, "SYS=VCS"
+
+seq hliClearanceSNC, "SYS=VCS"
+seq vliClearanceSNC, "SYS=VCS"
+seq trlClearanceSNC, "SYS=VCS"
+
