37a93489ccb5ec8694c5cbad467cfc4c6529f07c
yannS2016 <mandza.y.s@gmail.com>
2018-04-18 09:09:46 +0200

    Revert "Adding Testing IOC"
    This reverts commit 269ac697fd16755881df87e6d0a21bfef4262783.

--- VCSIOCAG10/vcsApp/src/O.linux-x86/xxxRecord.h.d
+++
@@ -1,6 +0,0 @@
-# DO NOT EDIT: This file created by mkmf.pl,v 1.5 2002/03/25 21:33:24 jba Exp $ 
-
-../O.Common/xxxRecord.h : /epics/bases/base-3.14.12.5/dbd/dbCommon.dbd
-
-
-../O.Common/xxxRecord.h : ../Makefile
