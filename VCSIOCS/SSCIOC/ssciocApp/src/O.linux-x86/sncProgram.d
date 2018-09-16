46d752abe08e6cc1caac29f7ce68bbd42dc1a0a7
yannS2016 <mandza.y.s@gmail.com>
2018-09-16 11:05:29 -0400

    32 bit SSC IOC builds

---
+++ VCSIOCS/SSCIOC/ssciocApp/src/O.linux-x86/sncProgram.d
@@ -0,0 +1,12 @@
+sncProgram.o: sncProgram.c /epics/support/seq-2-2-1/include/seq_snc.h \
+ /epics/support/seq-2-2-1/include/seqCom.h \
+ /epics/base/include/epicsTypes.h /epics/base/include/shareLib.h \
+ /epics/base/include/epicsThread.h \
+ /epics/base/include/os/Linux/osdThread.h /epics/base/include/shareLib.h \
+ /epics/base/include/epicsTime.h /epics/base/include/epicsTypes.h \
+ /epics/base/include/os/Linux/osdTime.h \
+ /epics/support/seq-2-2-1/include/pvAlarm.h \
+ /epics/support/seq-2-2-1/include/seq_release.h \
+ /epics/support/seq-2-2-1/include/seq_prim_types.h \
+ /epics/support/seq-2-2-1/include/seqMain.c /epics/base/include/iocsh.h \
+ /epics/base/include/epicsExport.h
